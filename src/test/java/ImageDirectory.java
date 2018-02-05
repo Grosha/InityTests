92300636e3347ba1fa37eededc7c80a54b1141e2
Andrey Grom <groshkka@zeoalliance.com>
2018-02-05 10:45:16 +0200

    remove files

--- src/test/java/ImageDirectory.java
+++
@@ -1,6 +0,0 @@
-import java.io.File;
-
-public interface ImageDirectory {
-    File classpathRoot = new File(System.getProperty("user.dir"));
-    File imgDir = new File(classpathRoot, "src/main/resources");
-}
